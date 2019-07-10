567dd3bb1ae7605d27fee2dc3a9da2f3397092c7
pradhsa <santosh.pradhan@tecnotree.com>
2019-07-10 15:34:30 +0530

    logger added

--- app.js
+++ app.js
@@ -5,8 +5,45 @@ var app = require('express')();
 var mongoose = require('mongoose');
 var bluebird = require('bluebird');
 var cors = require('cors');
+const addRequestId = require('express-request-id')();
+const morgan = require('morgan');
+var FileStreamRotator = require('file-stream-rotator');
+const fs = require('fs');
+
+var path = require('path');
+var logDirectory = path.join(require('app-root-dir').get(), 'logs');
 
 app.use(cors());
+app.use(addRequestId);
+
+morgan.token('id', function getId(req) {
+  return req.id
+});
+
+var loggerFormat = ':id [:date[web]] ":method :url" :status :response-time';
+//var loggerFormat = `[:date[web]] ${":method"}`;
+
+var _fileStream = FileStreamRotator.getStream({
+  filename: logDirectory + '/access-%DATE%.log',
+  date_format:('YYYYMMDD')
+})
+
+app.use(morgan(loggerFormat, {
+  skip: function (req, res) {
+      return res.statusCode < 400
+  },
+  stream: process.stderr
+}));
+
+app.use(morgan(loggerFormat, {
+  skip: function (req, res) {
+      return res.statusCode >= 400
+  },
+  stream: process.stdout
+}));
+
+//app.use(morgan('common', {stream: fs.createWriteStream('./access-%DATE%.log', {flags: 'a'})}));
+app.use(morgan('common', {stream: _fileStream}));
 
 module.exports = app; // for testing
 
